6682e7091fe177c0167d00ebb0131aaa8b57d3ed
Pasinb <panbemail@gmail.com>
2017-12-15 01:19:44 +0700

    Set DB to postgres

--- matchlink/matchlink/settings.py
+++ matchlink/matchlink/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'matchlink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': constants['DB_NAME'],
+        'USER': constants['DB_USER'],
+        'PASSWORD': constants['DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
